9c3cf7d81fd6adf3ecef20a1e9b02c0b2f3b559d
Bianca11810 <Bianca11810@gmail.com>
2022-09-13 00:49:13 +0000

    It worksgit add .

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+
+  console.log("Hello!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["the", "they"];
+  let subject = ["husband", "racoon", "loki", "driver", "mom"];
+  let action = ["ran my", "drove my", "threw my", "hid my", "hit my"];
+  let posession = ["toe", "laptop", "dog", "mom", "body"];
+  let where = ["home", "work", "chris office", "empire state building"];
+
+  let pronounindex = Math.floor(Math.random() * pronoun.length);
+  let subjectindex = Math.floor(Math.random() * subject.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let posessionindex = Math.floor(Math.random() * posession.length);
+  let whereindex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounindex] +
+    " " +
+    subject[subjectindex] +
+    " " +
+    action[actionindex] +
+    " " +
+    posession[posessionindex] +
+    " " +
+    where[whereindex]
+  );
 };
